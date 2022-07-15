5ef2c5738357235fbf828838d55f8ca850183ca4
somate10000 <98242447+somate10000@users.noreply.github.com>
2022-07-15 11:09:30 -0700

    Update App.js

--- App.js
+++ App.js
@@ -7,29 +7,10 @@ import {
 } from "@apollo/client";
 import TRX from "./components/TRX";
 
-const LAUNCHES_QUERYs = `
-{
-  launchesPast(limit:10){
-    id
-    mission_name
-  }
-}
-`;
-const LAUNCHES_QUERY = `
-{
-  date(id:"2020"){   
-  id
-  books{
-    name
-    genre
-  }  
-  }
-}
-`;
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "http://192.168.43.225:4000/graphql",
+  uri: "http://localhost:4000/graphql",
 });
 
 export default function App() {
